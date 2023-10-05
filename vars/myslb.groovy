3ffe910bbaf16a791561e195620a3a8f37221b53
suhair <75605826+Suhairkm@users.noreply.github.com>
2023-10-05 12:04:16 +0530

    Update myslb.groovy

--- vars/myslb.groovy
+++ vars/myslb.groovy
@@ -1,16 +1,4 @@
-// MySharedLibrary.groovy
-package com.example
-
-class MySharedLibrary implements Serializable {
-    def call(body) {
-        def config = [:]
-        body.resolveStrategy = Closure.DELEGATE_FIRST
-        body.delegate = config
-        body()
-        return this
-    }
-
-    def mySharedFunction() {
-        echo "Hello from MySharedLibrary!"
-    }
+def mySharedFunction(place) {
+        echo "Hello from {$place}!!!"
 }
+
